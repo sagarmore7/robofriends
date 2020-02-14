74dce0611feb5e2ef674bbdd141f2637d9221bae
sagarmore7 <56201579+sagarmore7@users.noreply.github.com>
2020-02-14 05:58:52 +0530

    updating

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const card = (props) =>{
+    const {name, email, id} = props;
+    return(
+      <div className='tc bg-light-green dib br3 pa2 ma2 grow bw2 shadow-5 cardd'>
+        <img alt='robots' src={`https://robohash.org/${id}?100x100`}/>
+          <div>
+             <h2>{name}</h2>
+             <p>{email}</p> 
+          </div>    
+      </div>      
+    )
+
+}
+
+export default card;
